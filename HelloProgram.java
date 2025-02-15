75e40b1642cb98c52396f1a1f156a5031009a7b9
pradip shinde <pradipshinde013@gmail.com>
2025-02-16 03:56:20 +0530

    Hello Jenkins Method added

--- HelloProgram.java
+++ HelloProgram.java
@@ -1,4 +1,7 @@
-
+/**
+	@author pradipshinde013
+	@date 16 FEB 2024
+*/
 public class HelloProgram {
 	
 	public void hello(){
@@ -6,9 +9,15 @@ public class HelloProgram {
 		System.out.println("Welcome to java 17");
 	}
 	
+	public void helloJenkins(){
+		System.out.println("Hello Pradip Shinde - JENKINS");
+		System.out.println("Welcome to java 17 - JENKINS BUILD SUCCESSFUL");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the main");
 		HelloProgram hp = new HelloProgram();
 		hp.hello();
+		hp.helloJenkins()
 	}
 }
